c4883c92cea72b43a13c67c91bcb8f89c9d809b8
Thomas Chen <foxnewsnetwork@gmail.com>
2013-05-10 15:33:35 -0700

    Used lein new to scaffold the guru project. Changed the project.clj a bit from the default test

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject guru "0.1.0-SNAPSHOT"
+  :description "/g/uru: /g/'s attempt to build an AI"
+  :url "http://4chan.org/g/"
+  :license {:name "4chan /g/ RMS Install Gentoo Muh Freedoms License"
+            :url "http://4chan.org/g/"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
